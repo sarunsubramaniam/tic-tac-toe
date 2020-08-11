e23f29dca4fb79364544747e5c044778893b84a4
Arun Subramaniam <arun@Aruns-MacBook-Air.local>
2020-08-11 22:45:28 +0530

    Updates

---
+++ precache-manifest.2f17edd73c514070293d3a868ab06b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909151c3f23774c8c424d51e150d5556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aaee48afc267835cb22",
+    "url": "/static/css/main.44b7b1aa.chunk.css"
+  },
+  {
+    "revision": "8e24858ceefbf3e31cbf",
+    "url": "/static/js/2.005abc25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.005abc25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aaee48afc267835cb22",
+    "url": "/static/js/main.52e94b4d.chunk.js"
+  },
+  {
+    "revision": "441cfea73f775386a3f5",
+    "url": "/static/js/runtime-main.02030f09.js"
+  }
+]);
